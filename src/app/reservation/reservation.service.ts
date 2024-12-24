9b684668fdf2a2a9f06662b4ec7c558e72e16eee
shreya valaboju <55218449+shreyavala@users.noreply.github.com>
2024-12-24 13:23:57 -0600

    update reservation service
    removed routing logic and changed to localstorage

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -14,58 +14,58 @@ export class ReservationService {
   private reservations: Reservation[] = [];
 
   // before ngOnInit lifecycle hook. when we start, immediately loads (for local storage logic)
-  // constructor() {
-  //   let savedReservations = localStorage.getItem("reservations")
-  //   this.reservations = savedReservations? JSON.parse(savedReservations):[];
-  //  } 
+  constructor() {
+    let savedReservations = localStorage.getItem("reservations")
+    this.reservations = savedReservations? JSON.parse(savedReservations):[];
+   } 
 
   //http / mock api:
-  private baseUrl="http://localhost:3001";
+  // private baseUrl="http://localhost:3001";
 
-  constructor(private http: HttpClient){}
+  //constructor(private http: HttpClient){}
 
 
   //this service provided CRUD services for our app. here are the below methods: 
 
   // observable means we're sending it out to an external api and waiting for the results. other events can subscrip/listen to the result. 
-  getReservations(): Observable<Reservation[]>{
-    //return this.reservations;
-    return this.http.get<Reservation[]>(this.baseUrl+"/reservations")
+  getReservations(): Reservation[] {
+    return this.reservations;
+    //return this.http.get<Reservation[]>(this.baseUrl+"/reservations")
   }
 
   // return a foudn reservation or an undefined value
-  getReservation(id: string): Observable<Reservation> | undefined{
-    //return this.reservations.find(res => res.id===id);
-    return this.http.get<Reservation>(this.baseUrl+"/reservation/"+id);
+  getReservation(id: string): Reservation | undefined{
+    return this.reservations.find(res => res.id===id);
+    //return this.http.get<Reservation>(this.baseUrl+"/reservations/"+id);
   }
 
-  addReservation(reservation: Reservation): Observable<void> {
+  addReservation(reservation: Reservation) :void  {
 
-    // reservation.id=Date.now().toString();
-    // this.reservations.push(reservation);
-    //localStorage.setItem("reservations",JSON.stringify(this.reservations))
+    reservation.id=Date.now().toString();
+    this.reservations.push(reservation);
+    localStorage.setItem("reservations",JSON.stringify(this.reservations))
 
-    return this.http.post<void>(this.baseUrl+"/reservation/",reservation);
+    // return this.http.post<void>(`${this.baseUrl}/reservations`, reservation);
 
   }
 
-  deleteReservation(id:string): Observable<void> {
+  deleteReservation(id:string):void {
 
-    // let index = this.reservations.findIndex(res => res.id === id)
-    // this.reservations.splice(index, 1); // remove element at index position
+    let index = this.reservations.findIndex(res => res.id === id)
+    this.reservations.splice(index, 1); // remove element at index position
 
-    return this.http.delete<void>(this.baseUrl+"/reservation/"+id);
+    // return this.http.delete<void>(this.baseUrl+"/reservations/"+id);
 
-    //localStorage.setItem("reservations",JSON.stringify(this.reservations))
+    localStorage.setItem("reservations",JSON.stringify(this.reservations))
   }
 
-  updateReservation(id: string, updatedReservation: Reservation): Observable<void> {
-    // let index = this.reservations.findIndex(res => res.id === id);
-    // this.reservations[index] = updatedReservation;
+  updateReservation(id: string, updatedReservation: Reservation):void {
+    let index = this.reservations.findIndex(res => res.id === id);
+    this.reservations[index] = updatedReservation;
 
-    //localStorage.setItem("reservations",JSON.stringify(this.reservations))
+    localStorage.setItem("reservations",JSON.stringify(this.reservations))
 
-    return this.http.put<void>(this.baseUrl+"/reservation/"+id,updatedReservation);
+    // return this.http.put<void>(this.baseUrl+"/reservations/"+id,updatedReservation);
   }
 
 }
